8c56293f051f1e6086209afc4211d41597d71098
Alexander Gladysh <agladysh@gmail.com>
2009-03-26 22:26:29 +0300

    initial commit

---
+++ lua/tserialize.lua
@@ -0,0 +1,6 @@
+-- tserialize.lua: Serialize arbitrary Lua data to Lua code
+-- This file is a part of lua-nucleo library
+-- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
+--
+
+-- TODO: Implement
