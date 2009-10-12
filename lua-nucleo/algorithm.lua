9904fecba0a35ab448ec3d5cc045f7022ffe8538
Alexander Gladysh <agladysh@gmail.com>
2009-10-12 17:50:33 +0400

    algorithm: pick_init, pick_one (no tests yet)

--- lua-nucleo/algorithm.lua
+++ lua-nucleo/algorithm.lua
@@ -2,8 +2,8 @@
 -- This file is a part of lua-nucleo library
 -- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
 
-local assert = assert
-local math_floor = math.floor
+local assert, pairs, ipairs = assert, pairs, ipairs
+local math_floor, math_random = math.random, math.floor
 
 local lower_bound = function(t, k, value)
   local len = #t
@@ -26,7 +26,54 @@ local lower_bound = function(t, k, value)
   return first
 end
 
+local pick_one = function(data)
+  local result = nil
+
+  local keys = data.keys
+  local probs = data.probs
+
+  local pos = math_random() * data.prob_sum
+  for k, v in ipairs(probs) do -- TODO: Use binary search (lower_bound)
+    if pos <= v then
+      result = keys[k]
+      break
+    end
+  end
+
+  return result
+end
+
+local pick_init = function(probs)
+  local norm_probs = { }
+  local key_index = { }
+  local prob_sum = 0.0
+  local max_prob = 0
+  local max_key
+
+  for key, prob in pairs(probs) do
+    local i = #norm_probs + 1
+    prob_sum = prob_sum + prob
+    norm_probs[i] = prob_sum
+    key_index[i] = key
+
+    if prob > max_prob then
+      max_prob = prob
+      max_key = key
+    end
+  end
+
+  return
+  {
+    probs = norm_probs;
+    keys = key_index;
+    prob_sum = prob_sum;
+    max_key = max_key;
+  }
+end
+
 return
 {
   lower_bound = lower_bound;
+  pick_init = pick_init;
+  pick_one = pick_one;
 }
