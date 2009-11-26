9945a42f1a3f63f155731860a7e80dafe962f952
Dmitry Potapov <dmitry.v.potapov@gmail.com>
2009-11-26 14:15:16 +0300

    test/priority_queue: refactored and added tests for many elements

--- test/priority_queue.lua
+++ test/priority_queue.lua
@@ -12,6 +12,12 @@ dofile('lua-nucleo/import.lua')
 local make_suite = select(1, ...)
 assert(type(make_suite) == "function")
 
+local is_table
+      = import 'lua-nucleo/type.lua'
+      {
+        'is_table'
+      }
+
 local assert_is_table,
       assert_is_number
       = import 'lua-nucleo/typeassert.lua'
@@ -22,20 +28,24 @@ local assert_is_table,
 
 local ensure,
       ensure_equals,
+      ensure_tdeepequals,
       ensure_fails_with_substring
       = import 'lua-nucleo/ensure.lua'
       {
         'ensure',
         'ensure_equals',
+        'ensure_tdeepequals',
         'ensure_fails_with_substring'
       }
 
 local tdeepequals,
-      tstr
+      tstr,
+      tclone
       = import 'lua-nucleo/table.lua'
       {
         'tdeepequals',
-        'tstr'
+        'tstr',
+        'tclone',
       }
 
 local make_priority_queue,
@@ -56,13 +66,17 @@ local check_insert_pop_elements = function(priority_queue, elements)
     priority_queue:insert(v.p, v.v);
   end
 
-  -- TODO: Sort elements somehow
-  local sorted_elements = elements
+  local sorted_elements = tclone(elements)
+  table.sort(sorted_elements, function(lhs, rhs) return lhs.p < rhs.p end)
 
   for i = 1, #sorted_elements do
     local popped_priority, popped_value = priority_queue:pop();
     ensure_equals("popped priority " .. i, popped_priority, sorted_elements[i].p);
-    ensure_equals("popped value " .. i, popped_value, sorted_elements[i].v);
+    if not is_table(popped_value) then
+      ensure_equals("popped value " .. i, popped_value, sorted_elements[i].v);
+    else
+      ensure_tdeepequals("popped table value " .. i, popped_value, sorted_elements[i].v);
+    end
   end
 
   ensure_equals("no first after element", priority_queue:front(), nil);
@@ -123,14 +137,42 @@ end)
 
 --------------------------------------------------------------------------------
 
-test "two-elements" (function()
+test "many-elements-direct" (function()
   local priority_queue = assert_is_table(make_priority_queue())
 
   local elements =
   {
-    { p = 1, v = 322} ;
+    { p = 1, v = 322 } ;
     { p = 2, v = "v" };
     { p = 3, v = function() end };
+    { p = 4, v = {} };
+  }
+
+  check_insert_pop_elements(priority_queue, elements)
+end)
+
+test "many-elements-reversed" (function()
+  local priority_queue = assert_is_table(make_priority_queue())
+
+  local elements =
+  {
+    { p = 4, v = {} };
+    { p = 3, v = function() end };
+    { p = 2, v = "v" };
+    { p = 1, v = 322 } ;
+  }
+
+  check_insert_pop_elements(priority_queue, elements)
+end)
+
+test "many-elements-random" (function()
+  local priority_queue = assert_is_table(make_priority_queue())
+
+  local elements =
+  {
+    { p = 2, v = "v" };
+    { p = 3, v = function() end };
+    { p = 1, v = 322 } ;
     { p = 4, v = {} };
   }
 
