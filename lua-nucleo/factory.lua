d1f2208fd076d27a983c7cf8aabf754658594b7e
Vladimir Fedin <vladfedin@yandex.ru>
2009-12-29 02:04:48 +0300

    lua-nucleo/factory.lua: file added

---
+++ lua-nucleo/factory.lua
@@ -0,0 +1,10 @@
+-- factory.lua -- factory related functions
+-- This file is a part of lua-nucleo library
+-- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
+
+local common_method_list = function() end
+
+return
+{
+  common_method_list = common_method_list;
+}
