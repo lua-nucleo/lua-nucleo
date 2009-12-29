f91c4e6542872b26885583df5c69317e08a8d5e0
Vladimir Fedin <vladfedin@yandex.ru>
2009-12-29 12:47:10 +0300

    test/factory.lua: nested objects, recursive table tests

--- test/factory.lua
+++ test/factory.lua
@@ -48,14 +48,11 @@ test "wrong_output" (function()
 end)
 
 local compare_lists = function(list_one, list_two)
-print(#list_one, #list_two) -- delete
   if #list_one ~= #list_two then return false end
   local number = #list_one
   for i = 1, number do
-print(list_one[i]) -- delete
     local is_found = false
     for j = 1, number do
-print(list_two[j]) -- delete
       if list_one[i] == list_two[j] then
         is_found = true
         break
@@ -135,11 +132,6 @@ test "no_arguments_number_key_method" (function()
       method_three = method_three;
     }
   end
-  local methods_list_true = {
-    1;
-    5;
-    "method_three";
-  }
   ensure_fails_with_substring(
       "wrong arguments",
       function() common_method_list(make_something) end,
@@ -166,11 +158,6 @@ test "no_arguments_table_key_method" (function()
       method_three = method_three;
     }
   end
-  local methods_list_true = {
-    "method_one";
-    {2};
-    "method_three";
-  }
   ensure_fails_with_substring(
       "wrong arguments",
       function() common_method_list(make_something) end,
@@ -179,9 +166,79 @@ test "no_arguments_table_key_method" (function()
 end)
 
 test "no_arguments_nested" (function()
+  local make_something = function()
+    local method_one = function(a, b, c)
+      return a + b + c
+    end
+    local method_two = function()
+      return 1
+    end
+    local method_three = function(a, b, c)
+      return a * b - c
+    end
+    local k = { method_two = 2 }
+
+    local make_inner_something = function()
+      local inner_method = function()
+        return 1
+      end
+      return { inner_method = inner_method }
+    end
+    local inner_table_one = make_inner_something()
+    local inner_table_two = make_inner_something()
+    return
+    {
+      inner_table_one = inner_table_one;
+      inner_table_two = inner_table_two;
+      method_one = method_one;
+      method_two = method_two;
+      method_three = method_three;
+    }
+  end
+  local methods_list_true = {
+    "method_one";
+    "method_two";
+    "method_three";
+  }
+  local methods_list_test = common_method_list(make_something)
+  assert(compare_lists(methods_list_true, methods_list_test))
 end)
 
 test "no_arguments_recursive" (function()
+  local make_something = function()
+    local method_one = function(a, b, c)
+      return a + b + c
+    end
+    local method_two = function()
+      return 1
+    end
+    local method_three = function(a, b, c)
+      return a * b - c
+    end
+    local k = { method_two = 2 }
+
+    local inner_recursive_table = {
+      "one";
+      "two";
+      "three";
+    }
+    inner_recursive_table[4] = inner_recursive_table
+
+    return
+    {
+      method_one = method_one;
+      method_two = method_two;
+      method_three = method_three;
+      inner_recursive_table = inner_recursive_table;
+    }
+  end
+  local methods_list_true = {
+    "method_one";
+    "method_two";
+    "method_three";
+  }
+  local methods_list_test = common_method_list(make_something)
+  assert(compare_lists(methods_list_true, methods_list_test))
 end)
 
 test "several_method" (function()
