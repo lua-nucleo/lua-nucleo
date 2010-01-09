fc2067a477c72d6d0332f2b9d9cf7da64b6b9fb4
Vladimir Fedin <vladfedin@yandex.ru>
2010-01-09 19:28:47 +0300

    test/suite-factory.lua: tests added

--- test/suite-factory.lua
+++ test/suite-factory.lua
@@ -8,10 +8,64 @@ dofile('lua-nucleo/import.lua')
 local make_suite = select(1, ...)
 assert(type(make_suite) == "function")
 
-do
-  local test = make_suite("test", {some_factory = true})
+local assert_is_number,
+      assert_is_string,
+      assert_is_table
+      = import 'lua-nucleo/typeassert.lua'
+      {
+        'assert_is_number',
+        'assert_is_string',
+        'assert_is_table'
+      }
+
+--------------------------------------------------------------------------------
+
+local make_another_factory = function(a, b, c)
+  assert_is_number(a)
+  assert_is_string(b)
+  assert_is_table(c)
+  local method1 = function()
+  end
+  local method2 = function()
+  end
+  local method3 = function()
+  end
+  return
+  {
+    method1 = method1,
+    method2 = method2,
+    method3 = method3
+  }
+end
+
+--------------------------------------------------------------------------------
+
+  local test = make_suite(
+      "test",
+      {
+        some_factory = true
+      }
+    )
   local var = 1
   test:factory "some_factory" {"method1", "method2", "method3"}
+  print "stuff"
+  assert(test:run() == false)
+  test:method "method1" (function()
+    var = 2
+  end)
+  assert(test:run() == false)
+  assert(var == 2)
+  test:methods "method2"
+               "method3"
+  assert(test:run() == true)
+
+  local test = make_suite(
+      "test",
+      {
+        make_another_factory = true
+      }
+    )
+  test:factory "make_another_factory" (make_another_factory, 1, "", {})
 
   assert(test:run() == false)
   test:method "method1" (function()
@@ -22,4 +76,3 @@ do
   test:methods "method2"
                "method3"
   assert(test:run() == true)
-end
