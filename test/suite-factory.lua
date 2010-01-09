0e9e70de1b842348940dbbd5fd77b0a279d6dcc9
Vladimir Fedin <vladfedin@yandex.ru>
2010-01-09 22:17:10 +0300

    test/suite-factory.lua: factory with no methods tests, formatting

--- test/suite-factory.lua
+++ test/suite-factory.lua
@@ -26,59 +26,78 @@ local ensure_equals
 
 --------------------------------------------------------------------------------
 
-local make_another_factory = function(a, b, c)
-  assert_is_number(a)
-  assert_is_string(b)
-  assert_is_table(c)
-  local method1 = function()
-  end
-  local method2 = function()
-  end
-  local method3 = function()
-  end
-  return
-  {
-    method1 = method1,
-    method2 = method2,
-    method3 = method3
-  }
+-- test:factory table input test
+do
+  local test = make_suite(
+      "test",
+      {
+        some_factory = true,
+        other_factory = true
+      }
+    )
+  local var = 1
+
+  test:factory "some_factory" { "method1", "method2", "method3" }
+  ensure_equals("test:run()", test:run(), false)
+
+  test:method "method1" (function() var = 2 end)
+  ensure_equals("test:run()", test:run(), false)
+  ensure_equals("var == 2", var, 2)
+
+  test:methods "method2" "method3"
+  ensure_equals("test:run()", test:run(), false)
+
+  test:factory "other_factory" { }
+  ensure_equals("test:run()", test:run(), true)
 end
 
 --------------------------------------------------------------------------------
 
-local test = make_suite(
-    "test",
+-- test:factory function input test
+do
+  local make_some = function()
+    return
     {
-      some_factory = true
+      method1 = 5,
+      method2 = "",
+      method3 = {}
     }
-  )
-local var = 1
-test:factory "some_factory" { "method1", "method2", "method3" }
+  end
 
-ensure_equals("test:run()", test:run(), false)
-test:method "method1" (function()
-  var = 2
-end)
-ensure_equals("test:run()", test:run(), false)
-ensure_equals("var == 2", var, 2)
-test:methods "method2"
-             "method3"
-ensure_equals("test:run()", test:run(), true)
-
-local test = make_suite(
-    "test",
+  local make_another = function(a, b, c)
+    assert_is_number(a)
+    assert_is_string(b)
+    assert_is_table(c)
+    local method1 = function() end
+    local method2 = function() end
+    local method3 = function() end
+    return
     {
-      make_another_factory = true
+      method1 = method1,
+      method2 = method2,
+      method3 = method3
     }
-  )
-test:factory "make_another_factory" (make_another_factory, 1, "", {})
+  end
 
-ensure_equals("test:run()", test:run(), false)
-test:method "method1" (function()
-  var = var + 2
-end)
-ensure_equals("test:run()", test:run(), false)
-ensure_equals("var", var, 4)
-test:methods "method2"
-             "method3"
-ensure_equals("test:run()", test:run(), true)
+  local test = make_suite(
+      "test",
+      {
+        make_some = true,
+        make_another = true
+      }
+    )
+
+  test:factory "make_another" (make_another, 1, "", {})
+  ensure_equals("test:run()", test:run(), false)
+
+  local var = 1
+  test:method "method1" (function() var = var + 2 end)
+  ensure_equals("test:run()", test:run(), false)
+  ensure_equals("var", var, 3)
+
+  test:methods "method2" "method3"
+  ensure_equals("test:run()", test:run(), false)
+
+  test:factory "make_some" (make_some)
+  ensure_equals("test:run()", test:run(), true)
+end
