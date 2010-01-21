1a7cfd5378d106eccb0b63155c1e2e5d18902bbb
Alexander Gladysh <agladysh@gmail.com>
2010-01-21 03:13:21 +0300

    test/lib/tserialize-test-utils: fixed style

--- test/lib/tserialize-test-utils.lua
+++ test/lib/tserialize-test-utils.lua
@@ -1,22 +1,20 @@
--- tserialize-test-utils.lua -- utility functions for tserialize testing
+-- tserialize-test-utils.lua: utility functions for tserialize testing
 -- This file is a part of lua-nucleo library
 -- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
 
--- ----------------------------------------------------------------------------
--- Test helper functions
--- ----------------------------------------------------------------------------
-assert(type(import) == "function", "Import is required to run")
-local tdeepequals = import("lua-nucleo/tdeepequals.lua") {'tdeepequals'}
-local tserialize = import 'lua-nucleo/tserialize.lua' {"tserialize"}
-local escape_string = import 'lua-nucleo/string.lua' {'escape_string'}
-local ensure_equals = import 'lua-nucleo/ensure.lua' {'ensure_equals'}
-local pack = import 'lua-nucleo/args.lua' {'pack'}
+local tdeepequals = import 'lua-nucleo/tdeepequals.lua' { 'tdeepequals' }
+local tserialize = import 'lua-nucleo/tserialize.lua' { 'tserialize' }
+local escape_string = import 'lua-nucleo/string.lua' { 'escape_string' }
+local ensure_equals = import 'lua-nucleo/ensure.lua' { 'ensure_equals' }
+local pack = import 'lua-nucleo/args.lua' { 'pack' }
 
 local check_fn_ok = function(eq, ...)
   local saved = tserialize(...)
   assert(type(saved) == "string")
-  print("saved length", #saved,
-    "(truncated to 100 chars, non-printable chars are urlencoded)")
+  print(
+      "saved length", #saved,
+      "(truncated to 100 chars, non-printable chars are urlencoded)"
+    )
   print(escape_string(saved:sub(1, 100)))
   local ne, expected = pack (...)
   local nl, loaded = pack(assert(loadstring(saved))())
@@ -24,7 +22,6 @@ local check_fn_ok = function(eq, ...)
   assert(eq(expected, loaded), "tserialize produced wrong table!")
   return saved
 end
-
 
 local check_ok = function(...)
   print("check_ok started")
@@ -40,5 +37,5 @@ end
 
 return
 {
-  check_ok=check_ok;
+  check_ok = check_ok;
 }
