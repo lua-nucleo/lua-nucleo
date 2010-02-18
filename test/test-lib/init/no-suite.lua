7b4f6678050a97822431b512f7b6a7c83ff57bba
Vladimir Fedin <vladfedin@yandex.ru>
2010-02-19 02:23:39 +0300

    test-lib/init/no-suite.lua, no-suite-no-import.lua: new init files added

---
+++ test/test-lib/init/no-suite.lua
@@ -0,0 +1,6 @@
+-- no-suite.lua: test initialization file
+-- This file is a part of lua-nucleo library
+-- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
+
+dofile('lua-nucleo/strict.lua')
+dofile('lua-nucleo/import.lua')
