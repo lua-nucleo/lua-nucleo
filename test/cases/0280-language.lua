77cd38ab415b8d0fbaddcfe8a91be5c3b45352d6
Vladimir Fedin <vladfedin@yandex.ru>
2010-02-19 15:17:20 +0300

    test/cases/0280-language.lua: heading comment changed

--- test/cases/0280-language.lua
+++ test/cases/0280-language.lua
@@ -1,4 +1,4 @@
--- 0280-language.lua: tests for lua-nucleo language
+-- 0280-language.lua: tests for lua-nucleo Lua language definitions
 -- This file is a part of lua-nucleo library
 -- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
 
