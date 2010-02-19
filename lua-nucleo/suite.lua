7fbb4cc6fbe3c9b35edbbad4304d19f4f93809d6
Vladimir Fedin <vladfedin@yandex.ru>
2010-02-19 12:47:38 +0300

    lua-nucleo/suite.lua: comment added

--- lua-nucleo/suite.lua
+++ lua-nucleo/suite.lua
@@ -2,7 +2,7 @@
 -- This file is a part of lua-nucleo library
 -- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
 
---dofile('lua-nucleo/strict.lua')
+--dofile('lua-nucleo/strict.lua') -- TODO: sandbox absence problem
 dofile('lua-nucleo/import.lua')
 
 local common_method_list
