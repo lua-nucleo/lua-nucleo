03a2cacf6dc55fed43fcc2377b811001d60c9dfc
Vladimir Fedin <vladfedin@yandex.ru>
2010-02-19 13:00:26 +0300

    test/cases/0270-ensure, 0280-language, 0290-tstr.lua: test:TODO added

--- test/cases/0280-language.lua
+++ test/cases/0280-language.lua
@@ -2,4 +2,10 @@
 -- This file is a part of lua-nucleo library
 -- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
 
+local make_suite = assert(loadfile('test/test-lib/init/strict.lua'))(...)
+
 -- TODO: write tests here
+local test = make_suite("language", { })
+test:TODO "language all tests"
+--------------------------------------------------------------------------------
+assert(test:run())
--- test/cases/0290-tstr.lua
+++ test/cases/0290-tstr.lua
@@ -1,5 +1,11 @@
--- 0290-tstr.lua: tests for lua-nucleo language
+-- 0290-tstr.lua: tests for visualization of non-recursive tables
 -- This file is a part of lua-nucleo library
 -- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
 
+local make_suite = assert(loadfile('test/test-lib/init/strict.lua'))(...)
+
 -- TODO: write tests here
+local test = make_suite("tstr", { })
+test:TODO "tstr all tests"
+--------------------------------------------------------------------------------
+assert(test:run())
