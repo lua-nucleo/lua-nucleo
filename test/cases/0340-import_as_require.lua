bc94976398738929ce13d243913d85ebc61127f6
Alexander Gladysh <agladysh@gmail.com>
2010-09-09 05:33:08 +0400

    test/import_as_require: using proper import() tests

--- test/cases/0340-import_as_require.lua
+++ test/cases/0340-import_as_require.lua
@@ -16,47 +16,11 @@ package.path = './?' .. package.path
 import = require('lua-nucleo.import_as_require').import
 
 local res, err = pcall(function()
-  -- TODO: Keep up-to-date somehow automatically.
-  local files =
-  {
-    'lua-nucleo/algorithm.lua';
-    'lua-nucleo/args.lua';
-    'lua-nucleo/assert.lua';
-    'lua-nucleo/checker.lua';
-    'lua-nucleo/coro.lua';
-    'lua-nucleo/deque.lua';
-    'lua-nucleo/ensure.lua';
-    'lua-nucleo/factory.lua';
-    'lua-nucleo/functional.lua';
-    -- 'lua-nucleo/import.lua';  -- Has side-effects, skipping
-    -- 'lua-nucleo/import_as_require.lua'; -- Has side-effects, skipping
-    'lua-nucleo/language.lua';
-    'lua-nucleo/log.lua';
-    'lua-nucleo/math.lua';
-    'lua-nucleo/misc.lua';
-    'lua-nucleo/prettifier.lua';
-    'lua-nucleo/priority_queue.lua';
-    'lua-nucleo/random.lua';
-    'lua-nucleo/sandbox.lua';
-    -- 'lua-nucleo/strict.lua'; -- Has side-effects, skipping
-    'lua-nucleo/string.lua';
-    -- 'lua-nucleo/suite.lua';  -- Has side-effects, skipping
-    'lua-nucleo/table-utils.lua';
-    'lua-nucleo/table.lua';
-    'lua-nucleo/tdeepequals.lua';
-    'lua-nucleo/timed_queue.lua';
-    'lua-nucleo/timestamp.lua';
-    'lua-nucleo/tpretty.lua';
-    'lua-nucleo/tserialize.lua';
-    'lua-nucleo/tstr.lua';
-    'lua-nucleo/type.lua';
-    'lua-nucleo/typeassert.lua';
-    'lua-nucleo/util/anim/interpolator.lua';
-  }
+  local test_import = assert(
+      assert(assert(loadfile("test/test-lib/import.lua"))())["test_import"]
+    )
 
-  for i = 1, #files do
-    import(files[i])
-  end
+  test_import("test/data/")
 end)
 
 import = nil -- Avoiding side-effects
