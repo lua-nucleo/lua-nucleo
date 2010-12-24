03a59ca6f7895700f1a020b823d10f967770f173
Alexander Gladysh <agladysh@gmail.com>
2010-12-24 09:50:34 +0300

    test/cases: stub cases for code/*

--- test/test-list.lua
+++ test/test-list.lua
@@ -5,6 +5,7 @@
 return
 {
   '0240-type';
+  '0420-code-foreign-globals-luajit2';
   '0140-random';
   '0300-generate-test-list';
   '0270-ensure';
@@ -22,15 +23,18 @@ return
   '0290-tstr';
   '0330-tpretty';
   '0080-functional';
+  '0410-code-profile';
   '0220-tserialize-basic';
   '0360-stack_with_factory';
   '0040-checker';
   '0120-misc';
+  '0430-code-foreign-globals-lua5_1';
   '0110-math';
   '0340-import_as_require';
   '0091-import-base_path';
   '0202-tdeepequals-autogen';
   '0070-factory';
+  '0400-code-exports';
   '0331-prettifier';
   '0380-scoped_cat_tree_manager';
   '0205-tdeepequals-other-functions';
@@ -44,6 +48,7 @@ return
   '0130-priority_queue';
   '0100-log';
   '0170-suite';
+  '0390-code-globals';
   '0231-tserialize-recursive';
   '0210-timed_queue';
   '0230-tserialize-metatables';
