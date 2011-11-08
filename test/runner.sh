325c74182c9f31419addce3d9d0045ba2ea1385d
Vladimir Dronnikov <dronnikov@gmail.com>
2011-11-08 20:25:09 +0400

    removed code which created extra rockspec while running tests

--- test/runner.sh
+++ test/runner.sh
@@ -4,8 +4,5 @@
 # This file is a part of lua-nucleo library
 # Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
 
-# Hack.
-lua etc/rockspec/generate.lua scm-N >rockspec/lua-nucleo-scm-N.rockspec
-
 lua "test/test-lib/generate-test-list.lua"
 lua "test/test.lua" "$@"
