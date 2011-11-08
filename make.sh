6005f9c00bc75edf78764ad1b5c8aec3fc033369
Vladimir Dronnikov <dronnikov@gmail.com>
2011-11-08 22:15:34 +0400

    removed banner-1.rockspec creation

--- make.sh
+++ make.sh
@@ -9,12 +9,10 @@ echo "----> Creating list-exports"
 etc/list-exports/list-exports list_all
 
 echo "----> Generating rockspecs"
-lua etc/rockspec/generate.lua banner-1 > rockspec/lua-nucleo-banner-1.rockspec
-
 lua etc/rockspec/generate.lua scm-1 > rockspec/lua-nucleo-scm-1.rockspec
 
 echo "----> Making rocks"
-sudo luarocks make rockspec/lua-nucleo-banner-1.rockspec
+sudo luarocks make rockspec/lua-nucleo-scm-1.rockspec
 
 echo "----> Restarting multiwatch and LJ2"
 sudo killall multiwatch && sudo killall luajit2
