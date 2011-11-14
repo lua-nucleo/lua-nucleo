bc7cd5b644eb3278fac3422e38703c17a854ff76
Alexander V. Nikolaev <avn@daemon.hole.ru>
2011-11-14 21:26:04 +0000

    Test to isolate bug #2304/#2317 in prettifier

--- test/cases/0331-prettifier.lua
+++ test/cases/0331-prettifier.lua
@@ -6,12 +6,14 @@ local make_suite = assert(loadfile('test/test-lib/init/strict.lua'))(...)
 
 local ensure,
       ensure_equals,
+      ensure_strequals,
       ensure_tequals,
       ensure_fails_with_substring
       = import 'lua-nucleo/ensure.lua'
       {
         'ensure',
         'ensure_equals',
+        'ensure_strequals',
         'ensure_tequals',
         'ensure_fails_with_substring'
       }
@@ -22,6 +24,8 @@ local make_prettifier,
       {
         'make_prettifier'
       }
+
+local table_concat = table.concat
 
 --------------------------------------------------------------------------------
 
@@ -34,6 +38,32 @@ test:factory("make_prettifier", make_prettifier)
 --------------------------------------------------------------------------------
 
 test:TODO "test make_prettifier"
+
+--------------------------------------------------------------------------------
+-- Test based on real bug scenario
+-- #2304 and #2317
+test "test broken behaivor" (function ()
+  local buf = {}
+  local indent = "  "
+  local cols = 80
+
+  -- initialize (as in tpretty())
+  local cat = function(v) buf[#buf + 1] = v end
+  local pr = make_prettifier(indent, buf, cols)
+
+  -- emulate failing serialization
+  pr:table_start()
+  pr:key_start()
+  cat("key")
+  pr:value_start()
+  cat("value")
+  pr:key_value_finish()
+  pr:table_finish()
+  pr:finished()
+
+  -- then check
+  ensure_strequals("not broken", table_concat(buf), "{ key = value }");
+end)
 
 --------------------------------------------------------------------------------
 
