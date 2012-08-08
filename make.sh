d0c49cf1734d84df9b2340155910e8327a21cb26
Alexander V. Nikolaev <avn@daemon.hole.ru>
2012-08-08 11:46:08 +0000

    make.sh: Add --no-restart option

--- make.sh
+++ make.sh
@@ -17,7 +17,9 @@ sudo luarocks remove --force lua-nucleo || true
 echo "----> Making rocks"
 sudo luarocks make rockspec/lua-nucleo-scm-1.rockspec
 
-echo "----> Restarting multiwatch and LJ2"
-sudo killall multiwatch || true ; sudo killall luajit2 || true
+if [[ $@ != *--no-restart* ]]; then
+  echo "----> Restarting multiwatch and LJ2"
+  sudo killall multiwatch || true ; sudo killall luajit2 || true
+fi
 
 echo "----> OK"
