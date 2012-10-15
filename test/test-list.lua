14de10405f7a24d774d610b9d3800eb4629d30e1
Alexey Romanov <jobmail37@gmail.com>
2012-10-15 11:36:21 +0400

    test/test-list: regenerated

--- test/test-list.lua
+++ test/test-list.lua
@@ -6,59 +6,220 @@
 
 return
 {
-  '0010-algorithm';
-  '0020-args';
-  '0030-assert';
-  '0040-checker';
-  '0050-coro';
-  '0051-pcall';
-  '0060-deque';
-  '0070-factory';
-  '0080-functional';
-  '0090-import';
-  '0091-import-base_path';
-  '0100-log';
-  '0110-math';
-  '0120-misc';
-  '0130-priority_queue';
-  '0140-random';
-  '0150-strict';
-  '0160-string';
-  '0170-suite';
-  '0171-suite-full';
-  '0180-table';
-  '0190-table-utils';
-  '0200-tdeepequals-basic-types';
-  '0201-tdeepequals-basic-tables';
-  '0202-tdeepequals-autogen';
-  '0203-tdeepequals-recursive';
-  '0204-tdeepequals-userdata-functions-threads';
-  '0205-tdeepequals-other-functions';
-  '0210-timed_queue';
-  '0220-tserialize-basic';
-  '0221-tserialize-autogen';
-  '0230-tserialize-metatables';
-  '0231-tserialize-recursive';
-  '0240-type';
-  '0250-typeassert';
-  '0260-util-anim-interpolator';
-  '0270-ensure';
-  '0280-language';
-  '0290-tstr';
-  '0300-generate-test-list';
-  '0310-sandbox';
-  '0320-timestamp';
-  '0330-tpretty';
-  '0331-prettifier';
-  '0340-import_as_require';
-  '0350-module';
-  '0360-stack_with_factory';
-  '0370-ordered_named_cat_manager';
-  '0380-scoped_cat_tree_manager';
-  '0390-code-globals';
-  '0400-code-exports';
-  '0410-code-profile';
-  '0420-code-foreign-globals-luajit2';
-  '0430-code-foreign-globals-lua5_1';
-  '0440-require_and_declare';
+  {
+    type = 'cases';
+    path = 'test/cases/0010-algorithm.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0020-args.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0030-assert.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0040-checker.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0050-coro.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0051-pcall.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0060-deque.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0070-factory.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0080-functional.lua';
+  };
+  {
+    type = 'low-level';
+    path = 'test/low-level/0090-import.lua';
+  };
+  {
+    type = 'low-level';
+    path = 'test/low-level/0091-import-base_path.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0100-log.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0110-math.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0120-misc.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0130-priority_queue.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0140-random.lua';
+  };
+  {
+    type = 'low-level';
+    path = 'test/low-level/0150-strict.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0160-string.lua';
+  };
+  {
+    type = 'suite';
+    path = 'test/suite/0170-suite-simple.lua';
+  };
+  {
+    type = 'suite';
+    path = 'test/suite/0171-suite-full.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0180-table.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0190-table-utils.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0200-tdeepequals-basic-types.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0201-tdeepequals-basic-tables.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0202-tdeepequals-autogen.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0203-tdeepequals-recursive.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0204-tdeepequals-userdata-functions-threads.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0205-tdeepequals-other-functions.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0210-timed_queue.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0220-tserialize-basic.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0221-tserialize-autogen.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0230-tserialize-metatables.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0231-tserialize-recursive.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0240-type.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0250-typeassert.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0260-util-anim-interpolator.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0270-ensure.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0280-language.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0290-tstr.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0300-generate-test-list.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0310-sandbox.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0320-timestamp.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0330-tpretty.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0331-prettifier.lua';
+  };
+  {
+    type = 'low-level';
+    path = 'test/low-level/0350-module.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0360-stack_with_factory.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0370-ordered_named_cat_manager.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0380-scoped_cat_tree_manager.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0390-code-globals.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0400-code-exports.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0410-code-profile.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0420-code-foreign-globals-luajit2.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0430-code-foreign-globals-lua5_1.lua';
+  };
+  {
+    type = 'cases';
+    path = 'test/cases/0440-require_and_declare.lua';
+  };
 }
