22983a4dc2f7d429237ff7a879418fdc9543892b
Alexander Altshuler <altexy@gmail.com>
2012-12-05 17:37:00 +0400

    test/low-level/0340-init: initial version

---
+++ test/low-level/0340-init.lua
@@ -0,0 +1,26 @@
+--------------------------------------------------------------------------------
+-- 0350-init.lua: tests for standalone lua-nucleo initialization
+-- This file is a part of lua-nucleo library
+-- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
+--------------------------------------------------------------------------------
+
+-- Intentionally not using test suite to avoid circular dependency questions.
+
+require 'lua-nucleo'
+
+assert(import ~= nil)
+assert(declare ~= nil)
+
+local test_import = assert(
+    assert(assert(loadfile("test/test-lib/import.lua"))())["test_import"]
+  )
+
+test_import("test/data/")
+
+local arguments
+      = import 'lua-nucleo/lua/args.lua'
+      {
+        'arguments',
+      }
+
+print("------> Init tests suite PASSED")
