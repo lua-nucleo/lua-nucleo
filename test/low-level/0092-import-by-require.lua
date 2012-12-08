21e11342c17abc6f31dd8ffef974735bcf6b876b
Alexander Altshuler <altexy@gmail.com>
2012-12-08 14:36:47 +0400

    test/low-level/0092-import-by-require: initial version

---
+++ test/low-level/0092-import-by-require.lua
@@ -0,0 +1,20 @@
+--------------------------------------------------------------------------------
+-- 0350-init.lua: tests for standalone lua-nucleo initialization
+-- This file is a part of lua-nucleo library
+-- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
+--------------------------------------------------------------------------------
+
+-- Intentionally not using test suite to avoid circular dependency questions.
+
+require 'lua-nucleo.import'
+
+assert(import ~= nil)
+assert(declare == nil)
+
+local test_import = assert(
+    assert(assert(loadfile("test/test-lib/import.lua"))())["test_import"]
+  )
+
+test_import("test/data/")
+
+print("------> Init tests suite PASSED")
