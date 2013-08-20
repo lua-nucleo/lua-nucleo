9f624d1fc510fc5ec99bf5dc830f9ba2145657a8
Alexander Gladysh <agladysh@gmail.com>
2013-08-20 11:57:15 +0400

    make-doc: newer LDoc uses `ldoc` executable instead of `ldoc.lua`

--- make-doc.sh
+++ make-doc.sh
@@ -1,2 +1,13 @@
 #! /bin/bash -e
-ldoc.lua . -a
+
+LDOC="$(which ldoc)" || true
+if [ -z "${LDOC}" ]; then
+  LDOC="$(which ldoc.lua)" || true
+fi
+
+if [ -z "${LDOC}" ]; then
+  echo "Error: `ldoc' or `ldoc.lua' executable not found" >&2
+  exit 1
+fi
+
+exec "${LDOC}" . -a
