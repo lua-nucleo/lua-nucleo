d710a59f04a933bf59b8b815e8bf2cba788bd826
Mark Gurevich <markgurevichster@gmail.com>
2013-08-24 12:19:13 +0400

    test/cases/0270-ensure: Fix file header

--- test/cases/0270-ensure.lua
+++ test/cases/0270-ensure.lua
@@ -1,5 +1,5 @@
 --------------------------------------------------------------------------------
--- 0031-ensure.lua: tests for enhanced assertions
+-- 0270-ensure.lua: tests for enhanced assertions
 -- This file is a part of lua-nucleo library
 -- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
 --------------------------------------------------------------------------------
