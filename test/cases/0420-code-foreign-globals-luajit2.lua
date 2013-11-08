3bf9af76020c57be3a68e53db3740926ef85ee96
Vladimir Fedin <vl.fedin@gmail.com>
2013-11-09 00:53:02 +0400

    test/cases/04*-code-foreign-globals-*: added test for missing _PROMPTs
    Signed-off-by: Vladimir Stebunov <vstebunov@gmail.com>

--- test/cases/0420-code-foreign-globals-luajit2.lua
+++ test/cases/0420-code-foreign-globals-luajit2.lua
@@ -1,13 +1,39 @@
 --------------------------------------------------------------------------------
--- 0420-code-foreign-globals-luajit2.lua:
+-- 0420-code-foreign-globals-luajit2.lua: test on foreign globals
 -- This file is a part of lua-nucleo library
 -- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
 --------------------------------------------------------------------------------
 
--- TODO: Add description!
--- TODO: Write tests!
--- #tmp679
+local make_suite = assert(loadfile('test/test-lib/init/strict-import-as-require.lua'))(...)
 
-local make_suite = assert(loadfile('test/test-lib/init/strict.lua'))(...)
+local shell_write,
+      shell_read
+      = import 'lua-aplicado/shell.lua'
+      {
+        'shell_write',
+        'shell_read'
+      }
+
+local ensure_equals
+      = import 'lua-nucleo/ensure.lua'
+      {
+        'ensure_equals'
+      }
+
+--------------------------------------------------------------------------------
+
 local test = make_suite("code_foreign_globals_luajit2")
+
 test:TODO "write tests"
+
+--------------------------------------------------------------------------------
+
+-- TODO: Add check for reading stderr and stdstr
+--       https://github.com/lua-aplicado/lua-aplicado/issues/34
+test 'global_variable__PROMPT_error_on_load_in_interactive_mode' (function ()
+  local ok, _ = pcall(shell_read, "which","luajit")
+  ensure_equals('Interpreter not found!', ok, true)
+  -- NOTE: _PROMPT is created when lua in interactive mode
+  local _, msg = pcall(shell_write, 'require "lua-nucleo"', "luajit", "-i")
+  ensure_equals('Lua failed on start in intarective mode', msg, nil)
+end)
--- test/cases/0430-code-foreign-globals-lua5_1.lua
+++ test/cases/0430-code-foreign-globals-lua5_1.lua
@@ -1,13 +1,35 @@
 --------------------------------------------------------------------------------
--- 0430-code-foreign-globals-lua5_1.lua:
+-- 0430-code-foreign-globals-lua5_1.lua: test on foreign globals
 -- This file is a part of lua-nucleo library
 -- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
 --------------------------------------------------------------------------------
 
--- TODO: Add description!
--- TODO: Write tests!
--- #tmp680
+local make_suite = assert(loadfile('test/test-lib/init/strict-import-as-require.lua'))(...)
 
-local make_suite = assert(loadfile('test/test-lib/init/strict.lua'))(...)
+local shell_write
+      = import 'lua-aplicado/shell.lua'
+      {
+        'shell_write'
+      }
+
+local ensure_equals
+      = import 'lua-nucleo/ensure.lua'
+      {
+        'ensure_equals'
+      }
+
+--------------------------------------------------------------------------------
+
 local test = make_suite("code_foreign_globals_lua5_1")
+
 test:TODO "write tests"
+
+--------------------------------------------------------------------------------
+
+-- TODO: Add check for reading stderr and stdstr
+--       https://github.com/lua-aplicado/lua-aplicado/issues/34
+test 'global_variable__PROMPT_error_on_load_in_interactive_mode' (function ()
+  -- NOTE: _PROMPT is created when lua in interactive mode
+  local _, msg = pcall(shell_write, 'require "lua-nucleo"', "lua", "-i")
+  ensure_equals('Lua failed on start in intarective mode', msg, nil)
+end)
