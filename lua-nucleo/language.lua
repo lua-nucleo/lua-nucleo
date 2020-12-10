8f3681c72ff73cbea3f78f0e4c09a5d46d46c64d
Alexey Romanoff <ar@logiceditor.com>
2020-12-10 15:38:22 +0300

    language: update to support all current Lua versions >= 5.1

--- lua-nucleo/language.lua
+++ lua-nucleo/language.lua
@@ -16,6 +16,11 @@ local lua51_keywords =
   ["true"] = true,    ["until"] = true,  ["while"] = true
 }
 
+local lua52_keywords = { ["goto"] = true }
+for k, _ in pairs(lua51_keywords) do
+  lua52_keywords[k] = true
+end
+
 local lua51_types =
 {
   ["nil"] = true;
@@ -28,8 +33,30 @@ local lua51_types =
   ["userdata"] = true;
 }
 
-return
+local language =
 {
   lua51_keywords = lua51_keywords;
+  lua52_keywords = lua52_keywords;
+  lua53_keywords = lua52_keywords;
+  lua54_keywords = lua52_keywords;
+
   lua51_types = lua51_types;
+  lua52_types = lua51_types;
+  lua53_types = lua51_types;
+  lua54_types = lua51_types;
 }
+
+for m = 1, 4 do
+  m = tostring(m)
+  if _VERSION == 'Lua 5.' .. m then
+    language.lua_types = language['lua5' .. m .. '_types']
+    language.lua_keywords = language['lua5' .. m .. '_keywords']
+    break
+  end
+end
+
+if not language.lua_types then
+  error('Unsupported Lua version: ' .. tostring(_VERSION))
+end
+
+return language
