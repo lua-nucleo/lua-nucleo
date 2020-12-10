eb83f3b58f44cf57345048333db17ea2a71e2359
Alexey Romanoff <ar@logiceditor.com>
2020-12-10 11:12:21 +0300

    test/cases/0240-type: mark "all_predicates" test as BROKEN if newproxy is not found

--- test/cases/0240-type.lua
+++ test/cases/0240-type.lua
@@ -4,6 +4,16 @@
 -- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
 --------------------------------------------------------------------------------
 
+local unpack = unpack or table.unpack
+local newproxy = newproxy or select(
+    2,
+    unpack({
+        xpcall(require, function() end,'newproxy')
+      })
+  )
+
+--------------------------------------------------------------------------------
+
 local make_suite = assert(loadfile('test/test-lib/init/strict.lua'))(...)
 
 local ensure,
@@ -35,35 +45,39 @@ test:tests_for 'is_nil'
 
 --------------------------------------------------------------------------------
 
-test "all_predicates" (function()
-  local is = type_imports
+if newproxy then
+  test "all_predicates" (function()
+    local is = type_imports
 
-  local values =
-  {
-    -- ["nil"] = nil; -- Can't do nil values in tables.
-    ["number"] = 42;
-    ["string"] = "lua-nucleo";
-    ["boolean"] = false;
-    ["table"] = {};
-    ["function"] = function() end;
-    ["thread"] = coroutine.create(function() end);
-    ["userdata"] = newproxy();
-  }
+    local values =
+    {
+      -- ["nil"] = nil; -- Can't do nil values in tables.
+      ["number"] = 42;
+      ["string"] = "lua-nucleo";
+      ["boolean"] = false;
+      ["table"] = {};
+      ["function"] = function() end;
+      ["thread"] = coroutine.create(function() end);
+      ["userdata"] = newproxy();
+    }
 
-  assert(is.is_self == is.is_table)
+    assert(is.is_self == is.is_table)
 
-  assert(is.is_nil(nil) == true)
+    assert(is.is_nil(nil) == true)
 
-  assert(is.is_type(42) == false)
-  assert(is.is_type("") == false)
+    assert(is.is_type(42) == false)
+    assert(is.is_type("") == false)
 
-  for name1, value1 in pairs(values) do
-    assert(is.is_type(name1) == true)
-    assert(is.is_nil(value1) == false)
-    for name2, value2 in pairs(values) do
-      assert(
-          assert(is["is_"..name1])(value2) == (name1 == name2)
-        )
+    for name1, value1 in pairs(values) do
+      assert(is.is_type(name1) == true)
+      assert(is.is_nil(value1) == false)
+      for name2, value2 in pairs(values) do
+        assert(
+            assert(is["is_"..name1])(value2) == (name1 == name2)
+          )
+      end
     end
-  end
-end)
+  end)
+else
+  test:BROKEN "all_predicates"
+end
