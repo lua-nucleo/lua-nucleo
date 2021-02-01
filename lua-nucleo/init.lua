bc94e8643a8d73a1046ed85cef2621b8e06dc5a0
Alexey Romanoff <ar@logiceditor.com>
2021-02-01 18:14:35 +0300

    lua-nucleo/init: export standard module metadata fields

--- lua-nucleo/init.lua
+++ lua-nucleo/init.lua
@@ -8,3 +8,13 @@
 require 'lua-nucleo.strict'
 require 'lua-nucleo.import'
 require = import 'lua-nucleo/require_and_declare.lua' { 'require_and_declare' }
+
+return
+{
+  _VERSION = '1.1.0';
+  _URL = 'https://github.com/lua-nucleo/lua-nucleo';
+  _COPYRIGHT = 'Copyright (C) 2009-2021 Lua-Núcleo authors';
+  _LICENSE = 'MIT (http://raw.githubusercontent.com/'
+          .. 'lua-nucleo/lua-nucleo/master/COPYRIGHT)';
+  _DESCRIPTION = 'A random collection of core and utility level Lua libraries';
+}
