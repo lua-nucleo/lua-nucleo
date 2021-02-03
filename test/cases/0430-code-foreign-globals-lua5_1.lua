9dfaa25a90cdce65950536d6810fcc4a82819597
Alexey Romanoff <ar@logiceditor.com>
2021-02-03 16:21:14 +0300

    test/cases/{0420-code-foreign-globals-luajit2, 0430-code-foreign-globals-lua5_1}: tests are BROKEN_IF cannot load lua-aplicado shell module

--- test/cases/0430-code-foreign-globals-lua5_1.lua
+++ test/cases/0430-code-foreign-globals-lua5_1.lua
@@ -23,23 +23,22 @@ test:TODO "write tests"
 local is_lua_aplicado_shell_found, err =
   pcall(import, 'lua-aplicado/shell.lua')
 
-if not is_lua_aplicado_shell_found then
-  test:BROKEN 'global_variable__PROMPT_error_on_load_in_interactive_mode'
-  return
-end
-
 --------------------------------------------------------------------------------
-
-local shell_write
-      = import 'lua-aplicado/shell.lua'
-      {
-        'shell_write'
-      }
 
 -- TODO: Add check for reading stderr and stdstr
 --       https://github.com/lua-aplicado/lua-aplicado/issues/34
-test 'global_variable__PROMPT_error_on_load_in_interactive_mode' (function ()
-  -- NOTE: _PROMPT is created when lua in interactive mode
-  local _, msg = pcall(shell_write, 'require "lua-nucleo"', "lua", "-i")
-  ensure_equals('Lua failed on start in intarective mode', msg, nil)
-end)
+test:BROKEN_IF(not is_lua_aplicado_shell_found)
+  'global_variable__PROMPT_error_on_load_in_interactive_mode' (
+    function ()
+      local shell_write
+            = import 'lua-aplicado/shell.lua'
+            {
+              'shell_write'
+            }
+
+      -- NOTE: _PROMPT is created when lua in interactive mode
+      local _, msg =
+        pcall(shell_write, 'require "lua-nucleo"', "lua", "-i")
+      ensure_equals('Lua failed on start in intarective mode', msg, nil)
+    end
+  )
