5acd41b66ffad99ce00780a04c175500f773f5f5
Serhi Breslavsky <bess.nebel@gmail.com>
2023-03-23 17:01:47 +0200

    lua-nucleo/ordered_keys: methods documentation added

--- lua-nucleo/ordered_keys.lua
+++ lua-nucleo/ordered_keys.lua
@@ -7,6 +7,12 @@
 
 local ordered_pairs = import 'lua-nucleo/tdeepequals.lua' { 'ordered_pairs' }
 
+--- Fill table values with keys indicies.
+-- @tparam table keys Table with keys.
+-- @treturn table keys_to_order Filled table.
+-- @usage
+-- keys_to_order({ 'a', 'b', 'c' })
+-- returns { a = 1, b = 2, c = 3 }
 local keys_to_order = function(keys)
   local keys_to_order = { }
 
@@ -17,6 +23,13 @@ local keys_to_order = function(keys)
   return keys_to_order
 end
 
+--- Create sort function for compare left and right side keys
+-- by index stored in value of key_order table or compare
+-- by itself if index does not exists for key.
+-- @tparam table key_order Table using for compare keys.
+-- @tparam[opt=false] boolean pre_ordered Indicates is key_order
+-- table presorted with keys_to_order method.
+-- @treturn function Sort function.
 local keys_less = function(key_order, pre_ordered)
   if not pre_ordered then
     key_order = keys_to_order(key_order)
@@ -46,6 +59,11 @@ local keys_less = function(key_order, pre_ordered)
   end
 end
 
+--- Create iterator for table t that sort table using keys_less method.
+-- @tparam table t Table to sort.
+-- @tparam table key_list See @{keys_less} for details.
+-- @tparam[opt=false] boolean pre_ordered See @{keys_less} for details.
+-- @return Iterator.
 local ordered_key_pairs = function(t, key_list, pre_ordered)
   return ordered_pairs(t, keys_less(key_list, pre_ordered))
 end
